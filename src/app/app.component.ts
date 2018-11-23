f6cd04e90d9e0a6d82f58b1c27e68c0650d94882
Iulian Coretchi <iulian.coretchi@gmail.com>
2018-11-23 15:38:28 +0200

    setup project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'nga-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'auto-stoc';
+  title = 'auto-stoc'
 }
