725ff990bcc074ce6fcae33bb6acc0060a962c6a
Iulian Coretchi <iulian.coretchi@gmail.com>
2018-11-26 12:34:39 +0200

    generated router-enabled modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppMaterialModule } from './app-material.module'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { InventoryModule } from './inventory/inventory.module'
+import { ManagerModule } from './manager/manager.module'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { UserModule } from './user/user.module'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AppMaterialModule],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    AppMaterialModule,
+    ManagerModule,
+    InventoryModule,
+    UserModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
